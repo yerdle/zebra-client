51b1144174d8d5eb9242c6b8f3e2562eb696147b
tashian <carl@tashian.com>
2014-04-02 15:02:31 -0700

    Reset the label size to 4x6 on startup

--- zebra.sh
+++ zebra.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+source .env
+echo "Resetting label size to 4x6 on $ZEBRA_PRINT_QUEUE_NAME"
+lpr -P $ZEBRA_PRINT_QUEUE_NAME -o raw < reset-label-size.zpl
+
 until coffee zebra.js.coffee; do
     echo "Zebra server crashed with exit code $?.  Respawning.." >&2
     sleep 1
